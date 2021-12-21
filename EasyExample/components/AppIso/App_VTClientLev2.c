0f324a77b9949253d2e1e044a0306e19a1b3f48d
Florian Lindmeyr <l0722@schueler.ms-muc.de>
2021-12-21 10:27:22 +0100

    Switch funktion

--- EasyExample/components/AppIso/App_VTClientLev2.c
+++ EasyExample/components/AppIso/App_VTClientLev2.c
@@ -184,12 +184,19 @@ void VTC_handleSoftkeysAndButtons_RELEASED(const struct ButtonActivation_S *pBut
 	setU32("CF-A", "Gesamtzaehler", Gesamtzaehler);
 
 	CheckZiel(pButtonData->u8Instance);
-	if(Gesamtzaehler==1)
+
+	switch(Gesamtzaehler)
+	{
+	case 1:
 		IsoVtcCmd_CtrlAudioSignal(pButtonData->u8Instance, 1, 440, 100, 500);
-	if(Gesamtzaehler==2)
-			IsoVtcCmd_CtrlAudioSignal(pButtonData->u8Instance, 2, 440, 100, 500);
-	if(Gesamtzaehler==3)
-			IsoVtcCmd_CtrlAudioSignal(pButtonData->u8Instance, 3, 440, 100, 500);
+	    break;
+   	case 2:
+   		IsoVtcCmd_CtrlAudioSignal(pButtonData->u8Instance, 2, 440, 100, 500);
+		break;
+   	case 3:
+   		IsoVtcCmd_CtrlAudioSignal(pButtonData->u8Instance, 3, 440, 100, 500);
+   		break;
+	}
 }
 
 void VTC_handleNumericValues(const struct InputNumber_S * pInputNumberData) {
